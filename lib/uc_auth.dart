e96c90d815acb95780cf660a61926842bc6ebbfd
jackson338 <jacksondoaks@gmail.com>
2025-05-30 09:36:35 -0600

    Fixed imports

--- lib/uc_auth.dart
+++ lib/uc_auth.dart
@@ -1,1 +1,2 @@
 export 'pages/auth_view.dart';
+export 'services/home_abstract_class.dart';
--- test/uc_auth_test.dart
+++ test/uc_auth_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:uc_auth/uc_auth.dart';
 
 void main() {
   test('adds one to input values', () {
