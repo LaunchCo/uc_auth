3404ff83e0cea55043e3800845be3e033660082f
jackson338 <jacksondoaks@gmail.com>
2025-05-30 10:39:59 -0600

    formatting

--- lib/pages/auth_view.dart
+++ lib/pages/auth_view.dart
@@ -41,29 +41,27 @@ class _AuthViewState extends State<AuthView> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Expanded(
-                          child: GestureDetector(
-                            onTap: () => setState(() {
-                              signIn = true;
-                            }),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: signIn
-                                    ? Colors.white
-                                    : Colors.grey.shade100,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(8),
-                                  topRight: Radius.circular(8),
-                                ),
+                        GestureDetector(
+                          onTap: () => setState(() {
+                            signIn = true;
+                          }),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: signIn
+                                  ? Colors.white
+                                  : Colors.grey.shade100,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(8),
+                                topRight: Radius.circular(8),
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Sign In',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                  ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'Sign In',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
                                 ),
                               ),
                             ),
