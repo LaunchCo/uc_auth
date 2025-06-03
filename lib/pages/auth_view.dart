8c1fdd204a77157d864e53ec71c23eb542d522ac
jackson338 <jacksondoaks@gmail.com>
2025-06-02 20:39:20 -0600

    formatting

--- lib/pages/auth_view.dart
+++ lib/pages/auth_view.dart
@@ -23,74 +23,49 @@ class _AuthViewState extends State<AuthView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-      body: SafeArea(
-        child: Center(
-          child: SizedBox(
-            width: kIsWeb ? MediaQuery.of(context).size.width * 0.5 : null,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool web = constraints.maxWidth <= 600;
+        return Scaffold(
+          backgroundColor: Colors.grey.shade200,
+          body: SafeArea(
+            child: Center(
+              child: SizedBox(
+                width: web ? MediaQuery.of(context).size.width * 0.5 : null,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            GestureDetector(
-                              onTap: () => setState(() {
-                                signIn = true;
-                              }),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: signIn
-                                      ? Colors.white
-                                      : Colors.grey.shade100,
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(8),
-                                    topRight: Radius.circular(8),
-                                  ),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-                                  child: Text(
-                                    'Sign In',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      color: Colors.black,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                GestureDetector(
+                                  onTap: () => setState(() {
+                                    signIn = true;
+                                  }),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: signIn
+                                          ? Colors.white
+                                          : Colors.grey.shade100,
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(8),
+                                        topRight: Radius.circular(8),
+                                      ),
                                     ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              child: GestureDetector(
-                                onTap: () => setState(() {
-                                  signIn = false;
-                                }),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: !signIn
-                                        ? Colors.white
-                                        : Colors.grey.shade100,
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(8),
-                                      topRight: Radius.circular(8),
-                                    ),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
                                       child: Text(
-                                        'Create Account',
+                                        'Sign In',
                                         style: TextStyle(
                                           fontSize: 20,
                                           color: Colors.black,
@@ -99,28 +74,58 @@ class _AuthViewState extends State<AuthView> {
                                     ),
                                   ),
                                 ),
-                              ),
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () => setState(() {
+                                      signIn = false;
+                                    }),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        color: !signIn
+                                            ? Colors.white
+                                            : Colors.grey.shade100,
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(8),
+                                          topRight: Radius.circular(8),
+                                        ),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Center(
+                                          child: Text(
+                                            'Create Account',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
+                            const SizedBox(height: 52),
+                            signIn
+                                ? SignIn(body: body, homeScreen: widget.homeScreen)
+                                : CreateAccount(
+                                    body: body,
+                                    homeScreen: widget.homeScreen,
+                                    createUserUrl: widget.app,
+                                  ),
+                            const SizedBox(height: 32),
                           ],
                         ),
-                        const SizedBox(height: 52),
-                        signIn
-                            ? SignIn(body: body, homeScreen: widget.homeScreen)
-                            : CreateAccount(
-                                body: body,
-                                homeScreen: widget.homeScreen,
-                                createUserUrl: widget.app,
-                              ),
-                        const SizedBox(height: 32),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
